5c6bff294dcf1ca26d46c8e2a3bf7923eff89f2c
Joseph Iglecias <igleciasjoseph@gmail.com>
2022-03-24 00:46:57 -0400

    Initial Commit

---
+++ SimpleWeatherApp/ContentView.swift
@@ -0,0 +1,120 @@
+//
+//  ContentView.swift
+//  SimpleWeatherApp
+//
+//  Created by Joseph Iglecias on 3/23/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var isNight = false
+    
+    var body: some View {
+        ZStack {
+            BackgroundView(isNight: $isNight)
+            
+            VStack(spacing:8) {
+                CityTextView(cityName: "Cupertino, CA")
+                
+                MainWeatherStatusView(imageName: isNight ? "moon.stars.fill" : "cloud.sun.fill", temperature: 76)
+                
+                HStack(spacing: 30) {
+                    WeatherDayView(dayOfWeek: "TUE", imageName: "cloud.sun.fill", temperature: 76)
+                    WeatherDayView(dayOfWeek: "WED", imageName: "sun.max.fill", temperature: 88)
+                    WeatherDayView(dayOfWeek: "THU", imageName: "wind.snow", temperature: 32)
+                    WeatherDayView(dayOfWeek: "FRI", imageName: "sunset.fill", temperature: 70)
+                    WeatherDayView(dayOfWeek: "SAT", imageName: "snow", temperature: 22)
+                }
+                
+                Spacer()
+                
+                Button {
+                    isNight.toggle()
+                } label: {
+                    WeatherButton(title: "Change Day Time", textColor: .blue, backgroundColor: .white)
+                }
+                
+                Spacer()
+            }
+        }
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct WeatherDayView: View {
+    
+    var dayOfWeek: String
+    var imageName: String
+    var temperature: Int
+    
+    var body: some View {
+        VStack {
+            Text(dayOfWeek)
+                .font(.system(size: 16, weight: .medium, design: .default))
+                .foregroundColor(.white)
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 40, height: 40)
+            
+            Text("\(temperature)°")
+                .font(.system(size: 28, weight: .medium))
+                .foregroundColor(.white)
+        }
+    }
+}
+
+struct BackgroundView: View {
+    
+    @Binding var isNight: Bool
+    
+    var body: some View {
+        LinearGradient(gradient: Gradient(colors: [isNight ? .black : .blue, isNight ? .gray : Color("lightBlue")]),
+                       startPoint: .topLeading,
+                       endPoint: .bottomTrailing)
+        .ignoresSafeArea()
+    }
+}
+
+
+struct CityTextView: View {
+    
+    var cityName: String
+    
+    var body: some View {
+        Text(cityName)
+            .padding()
+            .font(.system(size: 32, weight: .medium, design: .default))
+            .foregroundColor(.white)
+    }
+}
+
+struct MainWeatherStatusView: View {
+    
+    var imageName: String
+    var temperature: Int
+    
+    var body: some View {
+        VStack {
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 180, height: 180)
+            
+            Text("\(temperature)°")
+                .font(.system(size: 70, weight: .medium))
+                .foregroundColor(.white)
+        }
+        .padding(.bottom, 50)
+    }
+}
